4ac1e7e8292402aa9191db0a70c0fa8c189d6a37
hridoy <hasanhridoy1367@gmail.com>
2020-07-08 10:34:26 +0600

    Student form SetUp done

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+
+<?php include_once "vendor/autoload.php"; ?>
+<?php 
+    
+    use App\Controller\Student;
+
+    $student = new Student;
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +16,23 @@
 	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.css">
 </head>
 <body>
+    
+    <?php 
+
+       if ( isset($_POST['submit']) ) {
+           
+           //Get value manage
+           echo $name = $_POST['name'];
+           echo $email = $_POST['email'];
+           echo $cell = $_POST['cell'];
+
+           //file manage
+           $photo = $_FILES['photo'];
+
+       }
+
+     ?>
+
     <div class="container">
     	<div class="row">
     		<div class="card mt-3 mx-auto shadow-lg">
@@ -14,23 +41,22 @@
     				<a href="data.php" class="btn btn-sm btn-outline-info">View All Student</a>
     			</div>
     			<div class="card-body">
-    				<form action="">
+    				<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
     					
     					<label for="">Name</label>
-    					<input type="text" class="form-control" placeholder="Enter Your Name">
+    					<input type="text" class="form-control" placeholder="Enter Your Name" name="name">
 
     					<label for="">E_mail</label>
-    					<input type="text" class="form-control" placeholder="Enter Your Email">
+    					<input type="text" class="form-control" placeholder="Enter Your Email" name="email">
 
     					<label for="">Cell</label>
-    					<input type="text" class="form-control" placeholder="Enter Your Cell">
+    					<input type="text" class="form-control" placeholder="Enter Your Cell" name="cell">
 
     					<label for="">Photo</label>
-    					<br>
-    					<input type="file">
+    					<input type="file" class="form-control" name="photo">
     					<br>
 
-    					<input type="submit" class="btn btn-outline-info mt-2" value="Submit">
+    					<input type="submit" class="btn btn-outline-info mt-2" value="Submit" name="submit">
 
     				</form>
     			</div>
